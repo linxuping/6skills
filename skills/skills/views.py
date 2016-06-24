0be6218ee0e8d39e83dfedd641f66ea39b083442
linxuping <linxuping@126.com>
2016-06-24 00:45:43 -0700

    aja

--- skills/skills/views.py
+++ skills/skills/views.py
@@ -5,8 +5,28 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
 
+class TempMgr_base:  
+  #data
+  #methods
+  pass
+class TempMgr_register(TempMgr_base):  
+  pass
+class TempMgr_register_business(TempMgr_base):  
+  pass
+
+
+
 @csrf_exempt  
 def dispatch(request):
+  if request.is_ajax():
+    #method check and return json.
+    print request.GET  #pass args using "url: '/ajax?name=test',"
+    my_response = {'ajax_resp':'Hello, webapp World!'}
+    datos = json.dumps(my_response)
+    return HttpResponse(datos, mimetype='application/json')
+  else:
+    #method check and return obj.
+    pass
   print request.session.items()
   fp = open('templates/search.html')  
   t = Template(fp.read())  
