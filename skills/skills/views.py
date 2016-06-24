c9afdc8e1fa175903a202f711a8b320fed046682
linxuping <linxuping@126.com>
2016-06-24 01:53:58 -0700

    signin page jmp to index

--- skills/skills/views.py
+++ skills/skills/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.template import Template,Context,RequestContext
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 class TempMgr_base:  
   #data
@@ -12,6 +13,8 @@ class TempMgr_base:
 class TempMgr_register(TempMgr_base):  
   pass
 class TempMgr_register_business(TempMgr_base):  
+  pass
+class TempMgr_login(TempMgr_base):  
   pass
 
 
@@ -25,6 +28,7 @@ def dispatch(request):
     datos = json.dumps(my_response)
     return HttpResponse(datos, mimetype='application/json')
   else:
+    print request.POST
     #method check and return obj.
     pass
   print request.session.items()
@@ -37,12 +41,21 @@ def dispatch(request):
 
 @csrf_exempt  
 def login(request):
-  print request.session.items()
-  fp = open('templates/signin.html')  
-  t = Template(fp.read())  
-  fp.close()  
-  html = t.render(Context({"id":1}))  
-  return HttpResponse(html) 
+  print request.POST
+  if request.POST.get("username",None) == None:
+    print request.session.items()
+    fp = open('templates/signin.html')  
+    t = Template(fp.read())  
+    fp.close()  
+    html = t.render(Context({"id":1}))  
+    return HttpResponse(html) 
+  username = request.POST["username"]
+  password = request.POST["password"]
+  user = auth.authenticate(username=username, password=password)  
+  if user is not None and user.is_active:
+    return HttpResponseRedirect('/index') 
+  else:
+    return HttpResponseRedirect('/login') 
 
 
 @csrf_exempt  
