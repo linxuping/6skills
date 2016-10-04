3e16aa488b2de9dbb8ca61c153cf6b1d9045d31c
linxp <linxuping@126.com>
2016-10-04 20:36:51 +0800

    fix bug of acttype filter

--- skills/skills/wxapi.py
+++ skills/skills/wxapi.py
@@ -58,7 +58,7 @@ def activities_special_offers(req):
 	#	#sql_datefilter = "a.time_from<=DATE_ADD(NOW(),INTERVAL 2 WEEK) and "
 	#	sql_datefilter = "a.preinfo_id>0 and "
 	if acttype != "" and acttype != "全部" and acttype != "*":
-		sql_datefilter += "b.name = '%s' and "%acttype
+		sql_datefilter += "b2.name = '%s' and "%acttype
 	if area != "" and area != "全城" and area != "*":
 		sql_datefilter += "d.name = '%s' and "%area
 		
@@ -77,7 +77,7 @@ def activities_special_offers(req):
 	if not ret: #by date
 		_sql = "select a.id,imgs_act,title,b.name,c.name,age_from,age_to,a.price_child,a.price_adult,a.quantities_remain,img_cover,a.createtime,a2.price_child from 6s_activity a left join 6s_preinfo a2 on a.preinfo_id=a2.id left join 6s_acttype b on a.act_id=b.id left join 6s_position c on a.position_id=c.id where %s ((age_from between %d and %d) or (age_to between %d and %d) or (age_from<%d and age_to>%d)) and a.status=1 order by a.createtime desc limit %d offset %d;"%(sql_datefilter,_age_from,_age_to,_age_from,_age_to,_age_from,_age_to,pagesize,pagesize*(page-1) )
 	else: #by distr and date
-		_sql = "select * from ((select a.id,imgs_act,title,b.name as type,c.name,age_from,age_to,a.price_child as pchild,a.price_adult as padult,a.quantities_remain as qremains,img_cover,DATE_ADD(a.createtime,INTERVAL 12 MONTH) as sortdate,a2.price_child,a2.content from 6s_activity a left join 6s_preinfo a2 on a.preinfo_id=a2.id left join 6s_acttype b on a.act_id=b.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s b.status=1 and c.pid=%d and ((age_from between %d and %d) or (age_to between %d and %d) or (age_from<%d and age_to>%d)) and a.status=1)  union  (select a.id,imgs_act,title,b.name as type,c.name,age_from,age_to,a.price_child as pchild,a.price_adult as padult,a.quantities_remain as qremains,img_cover,a.createtime as sortdate,a2.price_child,a2.content from 6s_activity a left join 6s_preinfo a2 on a.preinfo_id=a2.id left join 6s_acttype b on a.act_id=b.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s b.status=1 and c.pid<>%d and ((age_from between %d and %d) or (age_to between %d and %d) or (age_from<%d and age_to>%d)) and a.status=1)) as tmptable order by sortdate desc limit %d offset %d;"%(sql_datefilter,position_id,_age_from,_age_to,_age_from,_age_to,_age_from,_age_to,sql_datefilter,position_id,_age_from,_age_to,_age_from,_age_to,_age_from,_age_to,pagesize,pagesize*(page-1) )
+		_sql = "select * from ((select a.id,imgs_act,title,b.name as type,c.name,age_from,age_to,a.price_child as pchild,a.price_adult as padult,a.quantities_remain as qremains,img_cover,DATE_ADD(a.createtime,INTERVAL 12 MONTH) as sortdate,a2.price_child,a2.content from 6s_activity a left join 6s_preinfo a2 on a.preinfo_id=a2.id left join 6s_acttype b on a.act_id=b.id left join 6s_acttype b2 on b.pid=b2.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s b.status=1 and c.pid=%d and ((age_from between %d and %d) or (age_to between %d and %d) or (age_from<%d and age_to>%d)) and a.status=1)  union  (select a.id,imgs_act,title,b.name as type,c.name,age_from,age_to,a.price_child as pchild,a.price_adult as padult,a.quantities_remain as qremains,img_cover,a.createtime as sortdate,a2.price_child,a2.content from 6s_activity a left join 6s_preinfo a2 on a.preinfo_id=a2.id left join 6s_acttype b on a.act_id=b.id left join 6s_acttype b2 on b.pid=b2.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s b.status=1 and c.pid<>%d and ((age_from between %d and %d) or (age_to between %d and %d) or (age_from<%d and age_to>%d)) and a.status=1)) as tmptable order by sortdate desc limit %d offset %d;"%(sql_datefilter,position_id,_age_from,_age_to,_age_from,_age_to,_age_from,_age_to,sql_datefilter,position_id,_age_from,_age_to,_age_from,_age_to,_age_from,_age_to,pagesize,pagesize*(page-1) )
 	count,rets=dbmgr.db_exec(_sql)
 	if count >= 0:
 		for i in range(count):
@@ -90,7 +90,7 @@ def activities_special_offers(req):
 		mo.logger.error("db fail. ")
 
 	#if district == "":
-	_sql = "select count(a.id) from 6s_activity a left join 6s_acttype b on a.act_id=b.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s ((age_from between %d and %d) or (age_to between %d and %d)) and a.status=1; "%(sql_datefilter,_age_from,_age_to,_age_from,_age_to)
+	_sql = "select count(a.id) from 6s_activity a left join 6s_acttype b on a.act_id=b.id left join 6s_acttype b2 on b.pid=b2.id left join 6s_position c on a.position_id=c.id left join 6s_position d on c.pid=d.id where %s ((age_from between %d and %d) or (age_to between %d and %d)) and a.status=1; "%(sql_datefilter,_age_from,_age_to,_age_from,_age_to)
 	#else:
 	#	_sql = "select count(a.id) from 6s_activity a left join 6s_acttype b on a.act_id=b.id left join 6s_position c on a.position_id=c.id where %s c.pid=(select id from 6s_position where name ='%s') and ((age_from between %d and %d) or (age_to between %d and %d)) and a.status=1; "%(sql_datefilter,area,_age_from,_age_to,_age_from,_age_to)
 	count,rets=dbmgr.db_exec(_sql)
@@ -1432,6 +1432,9 @@ def get_wx_payinfo(req):
 	ret,actid = check_mysql_arg_jsonobj("actid", req.GET.get("actid",None), "int")
 	if not ret:
 		return actid
+	ret,price = check_mysql_arg_jsonobj("price", req.GET.get("price",None), "int")
+	if not ret:
+		price = -1
 
 	_sql = "select a.title,a.price_child,b.price_child from 6s_activity a left join 6s_preinfo b on a.preinfo_id=b.id where a.id=%d;"%actid
 	count,rets=dbmgr.db_exec(_sql)
@@ -1439,7 +1442,7 @@ def get_wx_payinfo(req):
 		mo.logger.error("can't find activity: %d"%actid)
 		return response_json_error( "activity not exist." )
 	body = rets[0][0]
-	total_fee = rets[0][1] if rets[0][2] is None else rets[0][2]
+	total_fee = price if price>0 else (rets[0][1] if rets[0][2] is None else rets[0][2])
 	try:
 		_total_fee = int(float(total_fee)*100)
 		if _total_fee <= 0:
