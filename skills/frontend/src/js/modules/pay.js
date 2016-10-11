3fa306a350e2975f156d1200f663992b599e1a44
linxp <linxuping@126.com>
2016-10-11 14:04:52 +0800

    fix bug of pay price

--- skills/frontend/src/js/modules/pay.js
+++ skills/frontend/src/js/modules/pay.js
@@ -111,13 +111,12 @@ var Pay = React.createClass({
           <h3 className="name">总价</h3>
           <p className="price">
           ￥
-          {this.props.price ? this.props.price : (this.props.activity.sign_type == "3" ? 
-            sign3Price[this.props.major] : (this.props.activity.price_child_pre? this.props.activity.price_child_pre:this.props.activity.price_child) )}元</p>
+          {this.props.activity.sign_type == "3" ? sign3Price[this.props.major] : (this.props.activity.price_child_pre? this.props.activity.price_child_pre:this.props.activity.price_child)}元</p>
         </section>
         <div className="weui_btn_area mb20" 
           style={{position: "absolute", width: "100%", padding: 10, margin: 0, bottom: 0}}>
           <button id="paybtn" type="button" className="weui_btn weui_btn_primary" style={{height: 100}}
-            onClick={this.payHandler} data-price={this.props.activity.sign_type == "3" ? sign3Price[this.props.major] : this.props.activity.price_child}>支付</button>
+            onClick={this.payHandler} data-price={this.props.activity.sign_type == "3" ? sign3Price[this.props.major] : (this.props.activity.price_child_pre? this.props.activity.price_child_pre:this.props.activity.price_child)}>支付</button>
         </div>
       </div>
     );
