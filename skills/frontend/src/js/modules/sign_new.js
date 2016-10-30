0390607b6ac010e067993dc85b9096aca576f9e0
pavilion <pavilionking@qq.com>
2016-10-30 21:31:42 +0800

    选手年龄加到25

--- skills/frontend/src/js/modules/sign_new.js
+++ skills/frontend/src/js/modules/sign_new.js
@@ -119,7 +119,7 @@ var SignForm = React.createClass({
 	render: function() {
 		var age;
 		if (this.props.activity.sign_type == "3") {
-			ages = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+			ages = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 		} else {
 			ages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 		}
