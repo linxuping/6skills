031e654b65aaa79232201bd93a88cf0d5cea72f0
pavilion <pavilionking@qq.com>
2016-11-15 09:41:14 +0800

    config

--- skills/frontend/config.js
+++ skills/frontend/config.js
@@ -9,17 +9,19 @@ var srcAssets         = 'src';
 var developmentAssets = 'dist/static';
 // var productionAssets  = 'dist/production/assets';
 
-var proxyOptions = url.parse('http://www.6skills.com/activities');
+var proxyOptions = url.parse('http://www.6skills.com:19900/activities');
 proxyOptions.route = '/activities';
-var proxyOptions2 = url.parse('http://www.6skills.com/wx');
+var proxyOptions2 = url.parse('http://www.6skills.com:19900/wx');
 proxyOptions2.route = '/wx';
+var proxyOptions3 = url.parse('http://www.6skills.com:19900/get_wx_payinfo');
+proxyOptions3.route = '/get_wx_payinfo';
 
 module.exports = {
   browsersync: {
     development: {
       server: {
         baseDir: [build],
-        middleware: [proxy(proxyOptions), proxy(proxyOptions2)]
+        middleware: [proxy(proxyOptions), proxy(proxyOptions2), proxy(proxyOptions3)]
       },
       port: 9901,
       files: [
