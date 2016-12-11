1ec6f404e9386e86d304518d106d13d7a652a395
pavilion <pavilionking@qq.com>
2016-12-11 16:52:30 +0800

    openid

--- skills/frontend_wx/src/scripts/common/utils.js
+++ skills/frontend_wx/src/scripts/common/utils.js
@@ -4,7 +4,7 @@ let service = new Service();
 class Utils {
 
   getSearch (href) {
-    href = href || win.location.search;
+    href = href || window.location.search;
     var data = {},reg = new RegExp( "([^?=&]+)(=([^&]*))?", "g" );
     href && href.replace(reg,function( $0, $1, $2, $3 ){
       data[ $1 ] = $3;
@@ -13,9 +13,15 @@ class Utils {
   }
 
   getUrlParam(name) {
-  	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-  	var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-  	if (r != null) return unescape(r[2]); return null; //返回参数值
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var search = window.location.search || window.location.hash;
+    var r = search.split("?")[1];
+    if (r) {
+      r = r.match(reg)
+      return unescape(r[2]);
+    } else {
+      return null;
+    }
   }
 
   getopenid(){
@@ -24,42 +30,45 @@ class Utils {
     //   // return "oYgYJwcdR1mRcJ_3WLghpeTPJoKw"
     // }
     if (sessionStorage.getItem("6soid") == null){
-      alert("asdf")
-  		this.load_6soid();
-  		// if (arguments[0] == null)
-  		// 	this.try_jump_pubnum();
-  	}
-  	return sessionStorage.getItem("6soid");
+      this.load_6soid();
+    } else {
+      return sessionStorage.getItem("6soid");
+    }
   }
 
   /**
    * 获取openid
    */
-  load_6soid() {
+  load_6soid(that) {
     if (sessionStorage.getItem("6soid")){
-  		return;
-  	}
-    // if (this.getUrlParam("code")!=null && this.getUrlParam("state")!=null) {
+      return sessionStorage.getItem("6soid");
+    }
+    if (this.getUrlParam("code")!=null && this.getUrlParam("state")!=null) {
       service.fetch("/get_6sid", {code: this.getUrlParam("code")}, res=>{
         if (res.openid == null) {
           window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe6d40d1e6b8d010e&redirect_uri="+encodeURI(window.location.href)+"&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect";
         } else {
           sessionStorage.setItem("6soid",res.openid);
+          if (that) {
+            that.setState({loaded: true})
+          }
         }
+      }, (res)=>{
+        window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe6d40d1e6b8d010e&redirect_uri="+encodeURI(window.location.href)+"&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect";
       })
-    // }
+    }
   }
 
   jump_pubnum(){
-  	location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIzODU5NDE0Ng==&scene=123#wechat_redirect";
+    location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIzODU5NDE0Ng==&scene=123#wechat_redirect";
   }
 
   try_jump_pubnum(){
-  	if (sessionStorage.getItem("6soid") == null){
-  	  location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIzODU5NDE0Ng==&scene=123#wechat_redirect";
-  		return true;
-  	}
-  	return false;
+    if (sessionStorage.getItem("6soid") == null){
+      location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIzODU5NDE0Ng==&scene=123#wechat_redirect";
+      return true;
+    }
+    return false;
   }
 
   url2json(url) {
--- skills/frontend_wx/src/scripts/components/layout.jsx
+++ skills/frontend_wx/src/scripts/components/layout.jsx
@@ -1,20 +1,38 @@
 import React, {PropTypes} from 'react';
 import utils from '../common/utils';
+import Weui from 'react-weui';
+const {Toast} = Weui;
 
 export default class Layout extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  state = {
+    loaded: false
+  }
+
   componentDidMount() {
-    utils.load_6soid()
+    if (utils.getUrlParam("notNeedOpenid")) {
+      this.setState({
+        loaded: true
+      });
+    } else {
+      if (utils.getUrlParam("code")) {
+        utils.load_6soid(this)
+      } else {
+        this.setState({
+          loaded: true
+        });
+      }
+    }
   }
 
   render() {
     const {children} = this.props;
     return (
       <div className="container">
-        {children}
+        {this.state.loaded ? children : <Toast icon="loading" show={true}>加载中...</Toast>}
         <div id="msg-wrap"></div>
       </div>
     );
